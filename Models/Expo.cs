7ca7da14b514efc8dcc603c103fb05bd68bb1502
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-05-07 12:10:02 +0200

    Audio

--- Models/Expo.cs
+++ Models/Expo.cs
@@ -1,16 +1,20 @@
-﻿namespace ProjectRagnarock.Models
+﻿using NAudio.Wave;
+
+namespace ProjectRagnarock.Models
 {
     public class Expo
     {
         private string _name;
+        private AudioFileReader _audioFile;
         private string _description;
         private string _soundFilePath;
         private string _picturePath;
         private int _id;
         private static int _idCounter = 1;
 
-        public Expo(string name, string description, string soundFilePath, string picturePath)
+        public Expo(string name, string description,string soundFilePath, string picturePath, AudioFileReader audioFile)
         {
+            _audioFile = audioFile;
             _name = name;
             _description = description;
             _soundFilePath = soundFilePath;
