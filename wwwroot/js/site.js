26aaa94c36bff783ebf412dea5fe73f0055c82c1
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2024-05-28 11:31:40 +0200

    ??

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const popup = document.getElementById("validationPopup");
+    const closeBtn = document.getElementsByClassName("close")[0];
+    const validationSummary = document.querySelector('.popup-message-box');
+
+    if (validationSummary && validationSummary.innerHTML.trim() !== "") {
+        popup.style.display = "block";
+    }
+
+    closeBtn.onclick = function () {
+        popup.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+});
